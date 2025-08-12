8a9a73d7d6e35383241ab53d8c16cb5667dadb10
DamianToromanovic <d.toromanovic@web.de>
2025-08-12 13:08:33 +0200

    added 2 layout roots

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import "../globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-my-background text-my-foreground flex items-center justify-center min-h-screen">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div>{children}</div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
