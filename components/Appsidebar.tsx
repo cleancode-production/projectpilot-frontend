534addcffe13ed8a10d19617a910aea1335a8146
DamianToromanovic <d.toromanovic@web.de>
2025-08-12 15:32:57 +0200

    siebar

---
+++ components/Appsidebar.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import { IoHome, IoLayers } from "react-icons/io5";
+
+import { IoIosMail } from "react-icons/io";
+import { FaUserAlt, FaChevronUp, FaChevronDown } from "react-icons/fa";
+
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+import { useRef } from "react";
+import { Button } from "./ui/button";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: IoHome,
+    id: 1,
+  },
+  {
+    title: "Projekte",
+    url: "/projects",
+    icon: IoLayers,
+    id: 2,
+  },
+  {
+    title: "Benachrichtigungen",
+    url: "/notifications",
+    icon: IoIosMail,
+    id: 5,
+  },
+];
+
+export function AppSidebar() {
+  const sidebarRef = useRef(null);
+
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="py-4 ">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/">
+                <span className="font-bold text-2xl">Project-Pilot</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarSeparator className="bg-card" />
+
+      <SidebarContent className="bg-background text-my-foreground font-bold">
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <Button className="mt-10 flex justify-between bg-card text-foreground hover:bg-muted-foreground">
+                <div>Workspace</div>
+                <span className="text-4xl">
+                  <FaChevronUp />
+                  <FaChevronDown />
+                </span>
+              </Button>
+              {items.map((item) => (
+                <SidebarMenuItem className="my-4" key={item.id}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <span className="text-2xl text-gold">
+                        <item.icon />
+                      </span>
+                      <span className="text-xl">{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter className="bg-card">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton className="text-lg flex justify-between">
+                  <div className="flex items-center gap-2">
+                    <FaUserAlt />
+                    UserName
+                  </div>
+                  <FaChevronUp />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem className="text-lg">Account</DropdownMenuItem>
+                <DropdownMenuItem className="text-lg">
+                  Settings
+                </DropdownMenuItem>
+                <DropdownMenuItem className="text-lg text-red-500">
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "./ui/sidebar";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Moon, Sun } from "lucide-react";
+
+import { useTheme } from "next-themes";
+
+export default function Navbar() {
+  const { theme, setTheme } = useTheme();
+
+  console.log("Current theme:", theme); //make use of theme variable for lint suppression...
+
+  return (
+    <nav className="p-4 flex items-center justify-between sticky top-0 bg-background z-10">
+      <SidebarTrigger />
+      <div className="flex items-center gap-4">
+        <Link href={"/"}>Dashboard</Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+}
